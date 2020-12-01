fe8f8ae2a45aa97d1501e867b97c633a0e640011
BrianDaBrain11 <68790720+BrianDaBrain11@users.noreply.github.com>
2020-12-01 13:22:44 -0800

    Functioning Server, need to customize for game

--- MutineersGameServer/Program.cs
+++ MutineersGameServer/Program.cs
@@ -28,31 +28,31 @@ namespace MutineersGameServer
 			Console.Write("What port should I start on?");
 			port = Int32.Parse(Console.ReadLine());
 			if (port == 0)
-            {
+			{
 				port = 26950;
-            }
+			}
 
 			NetworkManager.Start(playerCount, port);
 		}
 
 		private static void MainThread()
-        {
+		{
 			Console.WriteLine($"Main thread started. Running at {Constants.TICKS_PER_SEC} ticks per second.");
 			DateTime _nextLoop = DateTime.Now;
 
 			while (isRunning)
-            {
+			{
 				while (_nextLoop < DateTime.Now)
-                {
+				{
 					//GameLogic.Update();
 					_nextLoop = _nextLoop.AddMilliseconds(Constants.MS_PER_TICK);
 
 					if (_nextLoop > DateTime.Now)
-                    {
+					{
 						Thread.Sleep(_nextLoop - DateTime.Now);
-                    }
-                }
-            }
-        }
+					}
+				}
+			}
+		}
 	}
 }
