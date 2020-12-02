0a54fa34d62ef37c6b5a71bbce2a0f9f34ebeae0
BrianDaBrain11 <68790720+BrianDaBrain11@users.noreply.github.com>
2020-12-02 09:53:52 -0800

    started working on game logic foundation, needs networking

--- MutineersGameServer/GameLogic.cs
+++ MutineersGameServer/GameLogic.cs
@@ -1,12 +1,107 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Timers;
 
-
-class GameLogic
+public class GameLogic
 {
-    public static void Update()
-    {
+	public static Timer aTimer;
+	public static bool gameRunning = false;
+	public static bool lobbyWaiting = true;
+	public static bool inRound = false;
+	public static int roundNumber = 0;
 
-    }
+	public static void Update()
+	{
+		DateTime timer = DateTime.Now;
+		if (!gameRunning)
+		{
+			Console.WriteLine("Waiting to start next round, hit enter to continue");
+			Console.Read();
+			gameRunning = true;
+			lobbyWaiting = true;
+			SetTimer(120000); //2 minutes
+		} 
+		else
+		{
+
+			if (inRound)
+			{
+				//logic update, send and receive packets
+
+
+				if (lobbyWaiting)
+				{
+					if (/*amount of people to start roung*/true)
+					{
+						lobbyWaiting = false;
+						SetTimer(false);
+						SetTimer(6000);
+					}
+				}
+				else if (!lobbyWaiting)
+				{
+					if (/*mutineer threshold past*/false)
+					{
+						GameEnd("Mutineers");
+					}
+
+					if (/*nomutineers*/false)
+					{
+						GameEnd("Pirates");
+					}
+					if (/*orderscompleted*/false)
+                    {
+						GameEnd("Pirates");
+                    }
+				}
+			}
+		}
+
+
+	}
+	public static void SetTimer(int _time)
+	{
+		aTimer = new Timer(_time);
+		aTimer.Elapsed += onTimedEvent;
+		aTimer.AutoReset = false;
+		aTimer.Enabled = true;
+	}
+	public static void SetTimer(bool _reset)
+	{
+		if (!_reset)
+		{
+			aTimer = new Timer();
+			aTimer.Enabled = false;
+		}
+	}
+
+	private static void onTimedEvent(Object source, ElapsedEventArgs e)
+	{
+		RoundEnd();
+	}
+
+	public static void RoundEnd()
+	{
+		//do round end Routine
+		//send vote packet
+		//receive vote packet
+		//send kill order to person who done died
+		if (/*mutineer threshold past*/false)
+		{
+			GameEnd("Mutineers");
+		}
+		
+		if (/*no mutineers left*/false)
+		{
+			GameEnd("Pirates");
+		}
+
+		SetTimer(60000);
+	}
+
+	public static void GameEnd(string winner)
+	{
+
+	}
 }
